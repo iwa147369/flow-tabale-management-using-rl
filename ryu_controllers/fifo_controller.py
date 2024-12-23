ef20c19ae2ba7f2a28017cfb5996290d4d4cc76d
Iwa <buiphuocthinh147369@gmail.com>
2024-12-23 19:34:54 +0800

    update fifo

--- ryu_controllers/fifo_controller.py
+++ ryu_controllers/fifo_controller.py
@@ -1,11 +1,19 @@
-from ryu.app.simple_switch_13 import SimpleSwitch13
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet, ether_types
 from collections import deque
 import time
 import colorlog
 
-class FIFOController(SimpleSwitch13):
+class FIFOController(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
     def __init__(self, *args, **kwargs):
         super(FIFOController, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
         self.flow_table = deque(maxlen=100)  # Track flow entries with FIFO queue
         self.max_flows = 100  # Maximum number of flows allowed
         
@@ -28,6 +36,18 @@ class FIFOController(SimpleSwitch13):
             extra={'color': 'green', 'bold': True}
         )
 
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Install table-miss flow entry
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                        ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
     def remove_flow(self, datapath, match):
         """Remove a specific flow entry"""
         ofproto = datapath.ofproto
@@ -36,7 +56,9 @@ class FIFOController(SimpleSwitch13):
         mod = parser.OFPFlowMod(
             datapath=datapath,
             command=ofproto.OFPFC_DELETE,
-            match=match
+            match=match,
+            out_port=ofproto.OFPP_ANY,
+            out_group=ofproto.OFPG_ANY
         )
         datapath.send_msg(mod)
         self.logger.info(
@@ -45,21 +67,22 @@ class FIFOController(SimpleSwitch13):
         )
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
-        self.logger.info(
-            f"Current flow table size: {len(self.flow_table)}/{self.max_flows}",
-            extra={'color': 'cyan'}
-        )
+        # First, check if this exact match already exists
+        for flow in self.flow_table:
+            if flow['match'] == match:
+                self.logger.info("Flow already exists, skipping addition")
+                return
 
-        # Check if flow table is full
+        # If flow table is full, remove oldest entry (FIFO policy)
         if len(self.flow_table) >= self.max_flows:
-            oldest_flow = self.flow_table.popleft()  # Remove oldest flow (FIFO)
+            oldest_flow = self.flow_table.popleft()
             self.logger.warning(
                 "Flow table full! Removing oldest entry...",
                 extra={'color': 'yellow', 'bold': True}
             )
             self.remove_flow(datapath, oldest_flow['match'])
-            
-        # Add new flow to our tracking table
+
+        # Add new flow
         flow_entry = {
             'match': match,
             'priority': priority,
@@ -67,10 +90,89 @@ class FIFOController(SimpleSwitch13):
         }
         self.flow_table.append(flow_entry)
         
+        # Install flow in switch
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        
+        if buffer_id:
+            mod = parser.OFPFlowMod(
+                datapath=datapath,
+                buffer_id=buffer_id,
+                priority=priority,
+                match=match,
+                instructions=inst,
+                hard_timeout=0,  # Flow entry never expires
+                flags=ofproto.OFPFF_SEND_FLOW_REM  # Request flow removal notification
+            )
+        else:
+            mod = parser.OFPFlowMod(
+                datapath=datapath,
+                priority=priority,
+                match=match,
+                instructions=inst,
+                hard_timeout=0,
+                flags=ofproto.OFPFF_SEND_FLOW_REM
+            )
+        
         self.logger.info(
-            f"Adding new flow - Priority: {priority}, Match: {match}",
+            f"Installing new flow - Priority: {priority}, Match: {match}",
             extra={'color': 'green'}
         )
-        
-        # Add the new flow to the switch using parent's add_flow
-        super().add_flow(datapath, priority, match, actions, buffer_id)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
+
+    @set_ev_cls(ofp_event.EventOFPFlowRemoved, MAIN_DISPATCHER)
+    def flow_removed_handler(self, ev):
+        msg = ev.msg
+        match = msg.match
+        self.logger.info(
+            f"Flow removed from switch: {match}",
+            extra={'color': 'yellow'}
+        )
